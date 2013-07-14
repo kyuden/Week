e6e69c8c9d3546b044b38a2d45f9e5409f17dab1
Kyuden <msmsms.um@gmail.com>
2013-07-14 14:32:01 +0900

    mail setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,16 @@ Week::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+    Week::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address:             "smtp.gmail.com",
+    port:                587,
+    domain:              "gmail.com",
+    authentication:      "plain",
+    user_name:           "msmsms.um@gmail.com",
+    password:            "2101dada",
+    enable_starttls_auto: true
+    }
+  end
 end
