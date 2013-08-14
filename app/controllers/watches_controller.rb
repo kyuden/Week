0374bc2c49b47d79711388abd1b29ce31a6fce41
Kyuden <msmsms.um@gmail.com>
2013-08-15 07:25:18 +0900

    add eader-loading and chash for watch

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,6 +1,9 @@
 class WatchesController < ApplicationController
   def index
-    @entries = Entry.where(watch_id: current_user.watch).page params[:page]
+    @entries = Entry.select("entries.id, entries.article_id")
+                    .includes(article: [:user, :comments])
+                    .where(watch_id: current_user.watch).page params[:page]
+
     @graph   = Koala::Facebook::API.new(current_user.access_token)
 
     respond_to do |format|
