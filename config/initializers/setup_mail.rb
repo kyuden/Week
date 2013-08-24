edcda02a3feddcc6113be7d88f51e2fb92b51a9a
Kyuden <msmsms.um@gmail.com>
2013-08-24 21:19:44 +0900

    add database.yml for staging

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,7 @@
 if Rails.env == 'development'
   Rails.application.routes.default_url_options[:host] = "localhost:3000"
-else
+elsif Rails.env == 'production'
   Rails.application.routes.default_url_options[:host] = "week-kyuden.herokuapp.com"
+else
+  Rails.application.routes.default_url_options[:host] = "week-kyuden-staging.herokuapp.com"
 end
