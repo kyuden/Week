ab417e5095abc4b1de2d31745a224666a083d382
Kyuden <msmsms.um@gmail.com>
2013-08-24 23:29:14 +0900

    fix session_store expire

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Week::Application.config.session_store :cookie_store, key: '_week_session'
+Week::Application.config.session_store :cookie_store, key: '_week_session', :expire_after => 2.months
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
